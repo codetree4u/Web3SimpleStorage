44b48cd1ec87920a98e45dee0380c9adbc62e90b
Carlos Raymundo <reachcarlostoday@gmail.com>
2021-09-26 18:20:32 -0700

    Update deploy.py
    send a signed transaction to the network

--- deploy.py
+++ deploy.py
@@ -48,7 +48,7 @@ abi = json.loads(
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 chain_id = 1337
 
-my_address = "0x81C7567367C8B7Bd876Ea35a45f6940546466bB8"
+my_address = "0x23a347aa017D9575d1Bc4A4219824310b6a59769"
 private_key = os.getenv("PRIVATE_KEY")
 
 
@@ -61,7 +61,14 @@ transaction = SimpleStorage.constructor().buildTransaction(
     {"chainId": chain_id, "from": my_address, "nonce": nonce}
 )
 
-
+# print(private_key)
 # Sign the transaction
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
-print(signed_txn)
+
+
+# Send it!
+tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
